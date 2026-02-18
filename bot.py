df3b70c36d9cc165454781f2c894d2317f6f7b3f
zamirailashova6-maker <zamirailashova6@gmail.com>
2026-02-18 07:50:22 +0500

    Foiz hisoblash funksiyasi qo'shildi

--- bot.py
+++ bot.py
@@ -1,30 +1,32 @@
 import telebot
 from sympy import sympify, solve
 
-# Telegram’dan olgan Tokeningni shu yerga qo‘y
-API_TOKEN = '8245715431:AAHlqcJT3ChhKfKOUrlSh_QwsRNttcVBThE'
+# Telegram'dan olgan Tokeningni shu yerga qo'y
+API_TOKEN = '8245715431:AAHlqcJT3ChhKfKOUrlSh_QwsRNttcVBT hE'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Salom! Men matematik botman. \nMisollarni yozing (masalan: 2+2 yoki x**2 - 4 = 0)")
+    bot.reply_to(message, "Salom! Men matematik botman. \nMisollarni yozing (masalan: 2+2 yoki 100*5% yoki x**2 - 4 = 0)")
 
 @bot.message_handler(func=lambda message: True)
 def calculate(message):
     try:
         text = message.text.lower()
+        
+        # Foiz belgisini /100 ga almashtiramiz
+        text = text.replace('%', '/100')
+        
         if '=' in text:
-            # Tenglamani yechish
             parts = text.split('=')
-            equation = sympify(parts[0].strip() + "- (" + parts[1].strip() + ")")
-            result = solve(equation)
-            bot.reply_to(message, f"Tenglama javobi: {result}")
+            equation = sympify(parts[0]) - sympify(parts[1])
+            res = solve(equation)
         else:
-            # Oddiy misolni hisoblash
-            result = sympify(text)
-            bot.reply_to(message, f"Natija: {result}")
+            res = sympify(text)
+            
+        bot.reply_to(message, f"Natija: {res}")
     except Exception as e:
-        bot.reply_to(message, "Kechirasiz, misolni tushunmadim. To'g'ri formatda yozing.")
+        bot.reply_to(message, "Xatolik! Misolni to'g'ri yozganingizni tekshiring.")
 
 bot.polling()
